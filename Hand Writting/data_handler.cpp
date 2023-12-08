2f9f741fd7553a6c7dad4c1a406f06891622b617
Eoghan Hynes <A00107408@student.ait.ie>
2023-12-08 09:49:14 +0000

    Update data_handler.cpp
    added main function in comments to the end of the file.

--- Hand Writting/data_handler.cpp
+++ Hand Writting/data_handler.cpp
@@ -219,3 +219,12 @@ int main()
 	dh->split_data();
 	dh->count_classes();
 }
+
+/*int main() {
+	data_handler* dh = new data_handler();
+
+	dh->read_feature_vector("train-images.idx3-ubyte");
+	dh->read_feature_labels("train-labels.idx1-ubyte");
+	dh->split_data();
+	dh->count_classes();
+}*/
